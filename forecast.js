b20824428ea4af160e6d953b3aba8474a5caf582
Jane Muthoni <54930887+muchirijane@users.noreply.github.com>
2020-05-09 00:00:32 +0300

    Add files via upload

---
+++ forecast.js
@@ -0,0 +1,36 @@
+const key = 'kRRgs69cyUctLSDkGXEj8JoqGdb6ID3G';
+
+//get weather condition
+const getWeather = async (id) =>{
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+}
+
+
+
+
+//get the city
+
+const getCity = async(city)=>{
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+
+  const data = await response.json();
+
+  return data[0];
+}
+
+getCity('london')
+  .then(data => getWeather(data.Key))
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
